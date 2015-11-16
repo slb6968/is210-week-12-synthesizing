6ca4d9631bb42ce6bf173875a023bf4fd6aed93b
Shavon Brown <slb6968@hotmail.com>
2015-11-15 22:08:41 -0500

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -3,10 +3,11 @@
 """Task 03 module"""
 
 def exception_test(arg1, arg2, arg3):
+    """function docstring"""
     caught = False
     try:
         arg1[arg2].index(arg3)
-    except:
+    except (TypeError, LookupError):
         caught = True
 
     return caught
